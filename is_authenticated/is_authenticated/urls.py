4ac5e9d1aae5cfec64e87ceb3f0f89ba8f3637ed
Kyle Johnston <kylerjohnston@gmail.com>
2020-11-14 11:30:04 -0500

    Authentication endpoint should be auth/

--- is_authenticated/is_authenticated/urls.py
+++ is_authenticated/is_authenticated/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.is_authenticated, name='is_authenticated'),
+    path('auth/', views.is_authenticated, name='is_authenticated'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
